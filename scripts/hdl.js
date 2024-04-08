8c71e5660335b933e11dd778316cc0103f001e64
Case <63287770+HS-Case@users.noreply.github.com>
2024-04-08 20:52:05 +0800

    Delete scripts/hdl.js

--- scripts/hdl.js
+++
@@ -1,4 +0,0 @@
-[MITM]
-activity-1.m.duiba.com.cn
-[rewrite_local]
-^https:\/\/activity-1\.m\.duiba\.com\.cn\/signactivity\/doSign$ url script-request-body https://raw.githubusercontent.com/chavyleung/scripts/master/haidilao/hdl.js
