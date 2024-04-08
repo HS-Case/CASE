e334503c4aa5694eab351cf0f33972da15516d7d
Case <63287770+HS-Case@users.noreply.github.com>
2024-04-08 20:42:53 +0800

    Create hdl.js

---
+++ scripts/hdl.js
@@ -0,0 +1,4 @@
+[MITM]
+activity-1.m.duiba.com.cn
+[rewrite_local]
+^https:\/\/activity-1\.m\.duiba\.com\.cn\/signactivity\/doSign$ url script-request-body https://raw.githubusercontent.com/chavyleung/scripts/master/haidilao/hdl.js
