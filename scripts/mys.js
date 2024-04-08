831d8617f0d8e92b8c2cda4f8b17f9752e6850d7
Case <63287770+HS-Case@users.noreply.github.com>
2024-04-08 21:01:54 +0800

    Update mys.js

--- scripts/mys.js
+++ scripts/mys.js
@@ -2,4 +2,4 @@
 api-takumi.mihoyo.com
 
 [rewrite_local]
-^https:\/\/api-takumi.mihoyo.com\/apihub\/api\/getGameList url script-request-header https://raw.githubusercontent.com/chavyleung/scripts/master/mihoyo/mihoyo.cookie.js
+^https:\/\/api-takumi.mihoyo.com\/apihub\/api\/getGameList url script-request-header mihoyo.cookie.js
