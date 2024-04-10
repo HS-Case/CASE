8ede067735dbaa9bdeeefe9a153d5530c9ac1d90
Case <63287770+HS-Case@users.noreply.github.com>
2024-04-10 11:27:05 +0800

    Delete scripts/dd_getToken.js

--- scripts/dd_getToken.js
+++
@@ -1,5 +0,0 @@
-[MITM]
-hostname = game.xiaojukeji.com
-
-[rewrite_local]
-^https?:\/\/game\.xiaojukeji\.com\/api\/game\/plant\/watering url script-request-body https://raw.githubusercontent.com/passerby-b/didi_fruit/main/dd_getToken.js
