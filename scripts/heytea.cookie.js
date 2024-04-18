c728010b43c792b43e82a936490d3430b6387b05
Case <63287770+HS-Case@users.noreply.github.com>
2024-04-18 18:17:11 +0800

    Create heytea.cookie.js

---
+++ scripts/heytea.cookie.js
@@ -0,0 +1,4 @@
+# mitm
+hostname = go.heytea.com
+# 喜茶Go获取cookie 
+^https:\/\/go\.heytea\.com\/api\/service-member\/vip\/member-exp\/account-infourl script-response-body https://raw.githubusercontent.com/xzxxn777/Surge/main/Script/HeyTea/HeyTea.js
