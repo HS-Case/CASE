1cf8f0afb599c93550602c3bfb6ed7edee75b11a
Case <63287770+HS-Case@users.noreply.github.com>
2024-04-20 17:40:19 +0800

    Delete scripts/heytea.cookie.js

--- scripts/heytea.cookie.js
+++
@@ -1,4 +0,0 @@
-# mitm
-hostname = go.heytea.com
-# 喜茶Go获取cookie 
-^https:\/\/go\.heytea\.com\/api\/service-member\/vip\/member-exp\/account-infourl script-response-body https://raw.githubusercontent.com/xzxxn777/Surge/main/Script/HeyTea/HeyTea.js
