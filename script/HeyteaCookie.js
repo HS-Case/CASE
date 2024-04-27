f88a5aa137cfa162ca81b73c6b329d93fdb00220
Case <63287770+HS-Case@users.noreply.github.com>
2024-04-27 15:34:02 +0800

    Create HeyteaCookie.js

---
+++ script/HeyteaCookie.js
@@ -0,0 +1,5 @@
+[MITM]
+hostname = go.heytea.com
+
+[rewrite_local]
+^https:\/\/go\.heytea\.com\/api\/service-member\/vip\/member-exp\/account-info url script-response-body https://raw.githubusercontent.com/xzxxn777/Surge/main/Script/HeyTea/HeyTea.js
