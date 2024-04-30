a00336bef1c2475ff5eae3806d3f6a341c6b50f6
Case <63287770+HS-Case@users.noreply.github.com>
2024-04-30 09:27:59 +0800

    Update TongYiCookie.js

--- scripts/TongYiCookie.js
+++ scripts/TongYiCookie.js
@@ -1,2 +1,2 @@
-统一_获取token = type=http-response,pattern=^https:\/\/xapi\.weimob\.com\/api3\/onecrm\/user\/center\/usercenter\/queryUserInfo,script-path=https://raw.githubusercontent.com/xzxxn777/Surge/main/Script/TongYi/TongYi.js,requires-body=1,max-size=0
-hostname = %APPEND% xapi.weimob.com
+统一Cookie = type=http-response,pattern=^https:\/\/xapi\.weimob\.com\/api3\/onecrm\/user\/center\/usercenter\/queryUserInfo,script-path=https://raw.githubusercontent.com/xzxxn777/Surge/main/Script/TongYi/TongYi.js,requires-body=1,max-size=0
+hostname = xapi.weimob.com
