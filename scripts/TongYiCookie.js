857a82b9c7a153e975b09f7366a8922aa545ef94
Case <63287770+HS-Case@users.noreply.github.com>
2024-04-30 09:44:08 +0800

    Update TongYiCookie.js

--- scripts/TongYiCookie.js
+++ scripts/TongYiCookie.js
@@ -1,4 +1,2 @@
-[rewrite_local]
 ^https:\/\/xapi\.weimob\.com\/api3\/onecrm\/user\/center\/usercenter\/queryUserInfo url script-response-body https://raw.githubusercontent.com/xzxxn777/Surge/main/Script/TongYi/TongYi.js
-[MITM]
 hostname = xapi.weimob.com
