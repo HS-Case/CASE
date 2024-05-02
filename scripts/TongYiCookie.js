8ae8ce431a9d0d6e9baba5d1b06d91272e523fc1
Case <63287770+HS-Case@users.noreply.github.com>
2024-05-02 10:37:17 +0800

    Delete scripts/TongYiCookie.js

--- scripts/TongYiCookie.js
+++
@@ -1,6 +0,0 @@
-
-# mitm
-hostname = xapi.weimob.com
-
-# 获取蜜雪冰城需要的各项参数
-^https:\/\/xapi\.weimob\.com\/api3\/onecrm\/user\/center\/usercenter\/queryUserInfo url script-response-body https://raw.githubusercontent.com/xzxxn777/Surge/main/Script/TongYi/TongYi.js
