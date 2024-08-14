efb66bdaf01cdb74b08a9c4c58c7777a9e1b6bc7
Case <63287770+HS-Case@users.noreply.github.com>
2024-08-14 13:53:33 +0800

    Delete scripts/sfexpress.cookie.js

--- scripts/sfexpress.cookie.js
+++
@@ -1,4 +0,0 @@
-# mitm
-hostname = ccsp-egmas.sf-express.com
-# 顺丰速运获取cookie 
-^https:\/\/ccsp-egmas.sf-express.com\/cx-app-member\/member\/app\/user\/universalSign url script-request-body https://raw.githubusercontent.com/chavyleung/scripts/master/sfexpress/sfexpress.cookie.js
