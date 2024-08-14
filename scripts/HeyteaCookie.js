a2e6f8196852b56fb8302414a59e419eb846fa67
Case <63287770+HS-Case@users.noreply.github.com>
2024-08-14 13:54:10 +0800

    Delete scripts/HeyteaCookie.js

--- scripts/HeyteaCookie.js
+++
@@ -1,5 +0,0 @@
-[rewrite_local]
-^https:\/\/vip.heytea.com\/api\/service-member\/vip\/task\/member url script-response-body https://gist.githubusercontent.com/Sliverkiss/cc0928ca661cf3d89f55902e11b28432/raw/heytea.js
-
-[MITM]
-hostname = vip.heytea.com
