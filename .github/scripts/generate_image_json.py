ded8847f593d111c7b33c863e00b3f7dcb65c58a
Case <63287770+HS-Case@users.noreply.github.com>
2024-10-10 22:49:01 +0800

    Update and rename generate_image_json.py to generate_image_json.py

--- .github/scripts/generate_image_json.py
+++ .github/scripts/generate_image_json.py
@@ -6,7 +6,7 @@ def generate_json():
     json_data = {
         "name": "Case图标订阅",
         "description": "自用图标",
-        "icons": []
+        "icons": "https://github.com/HS-Case/CASE/raw/main/icon/Case.png"
     }
 
     for filename in os.listdir(image_folder):
