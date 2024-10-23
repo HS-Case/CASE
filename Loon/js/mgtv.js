dac74894fba1019a262e317a2c8657f7403337fd
Case <63287770+HS-Case@users.noreply.github.com>
2024-10-23 19:26:36 +0800

    Update mgtv.js

--- Loon/js/mgtv.js
+++ Loon/js/mgtv.js
@@ -1,7 +1,11 @@
 
 var url = $request.url,
   updatedUrl = url;
-if (url.includes("video/getSource")) updatedUrl = url.replace(/([?&]ticket=)\w{32}/, "$13B85D0CBD79B324CA10F3E21ECFAC8F1");else url.includes("video/source") && (updatedUrl = url.replace(/([?&]ticket=)\w{32}/, "$13B85D0CBD79B324CA10F3E21ECFAC8F1"));
+if (url.includes("video/getSource")) {
+  updatedUrl = url.replace(/([?&]ticket=)\w{32}/, "$13B85D0CBD79B324CA10F3E21ECFAC8F1");
+} else if (url.includes("video/source")) {
+  updatedUrl = url.replace(/([?&]ticket=)\w{32}/, "$13B85D0CBD79B324CA10F3E21ECFAC8F1");
+}
 $done({
   "url": updatedUrl
 });
